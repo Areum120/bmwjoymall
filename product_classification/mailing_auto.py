2ff745f2f652a2c46c497039e1fbdb4dd9008bec
LAPTOP-F3LFLS66\tjfsu <tjfsu120@gmail.com>
2022-01-15 23:56:39 +0900

    대량메일 자동발송 class로 수정

--- product_classification/mailing_auto.py
+++ product_classification/mailing_auto.py
@@ -3,47 +3,55 @@ from smtplib import SMTP_SSL
 from pathlib import Path
 from openpyxl import load_workbook
 
+class SendEmail:
+    #init은 인스턴스 객체 초기화
+    def __init__(self, id, pw, df):
+        # 로그인 계정/pw
+        self.id = id
+        self.pw = pw  # 앱비밀번호16자리
+        # 엑셀파일에서 가져온 정보를 활용해 함수 반복 실행
+        self.df = df
 
+    def send_email(self):
+        #email_list 실행
+        wb = load_workbook(self.df, data_only=True)
+        ws = wb.active
 
-def send_email(id, pw, recipient, title, text, attachment=False):
-    # 템플릿 생성
-    msg = EmailMessage()
+        #read_email_list, for문 안에서 email 발송
+        for row in ws.iter_rows():
+            recipient = row[0].value
+            print(recipient)
+            title = row[1].value
+            text = row[2].value
+            attachment = row[3].value
 
-    # 로그인 계정/pw
-    MY_ID = id
-    MY_PW = pw# 앱비밀번호16자리
+            # 템플릿 생성
+            msg = EmailMessage()
 
-    # 보내는 사람 / 받는 사람 / 제목 입력
-    msg["From"] = MY_ID
-    msg["To"] = recipient.split(",")
-    msg["Subject"] = title
+            # 보내는 사람 / 받는 사람 / 제목 입력
+            msg["From"] = self.id
+            msg["To"] = recipient.split(",")
+            msg["Subject"] = title
 
-    # 본문 구성
-    msg.set_content(text)
+            # 본문 구성
+            msg.set_content(text)
 
-    # 파일 첨부
-    if attachment:
-        filenm = Path(attachment).name
-        with open('./data/'+attachment, 'rb') as f:
-            msg.add_attachment(f.read(), maintype='application', subtype='octet-stream', filename=filenm)
+            # 파일 첨부
+            if attachment:
+                filenm = Path(attachment).name
+                with open('./data/'+attachment, 'rb') as f:
+                    msg.add_attachment(f.read(), maintype='application', subtype='octet-stream', filename=filenm)
 
-    # 보내는 사람 로그인 및 smtp 서버로 발송
-    with SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(MY_ID, MY_PW)
-        smtp.send_message(msg)
-    # 완료 메시지
-    print("발송 성공")
+            # 보내는 사람 로그인 및 smtp 서버로 발송
+            with SMTP_SSL("smtp.gmail.com", 465) as smtp:
+                smtp.login(self.id, self.pw)
+                smtp.send_message(msg)
+            # 완료 메시지
+            print("발송 성공")
 
-# 엑셀파일에서 가져온 정보를 활용해 함수 반복 실행
-wb = load_workbook('email_list.xlsx', data_only=True)
-ws = wb.active
+#인스턴스 생성
+es = SendEmail('**', '**', 'email_list.xlsx')
 
-for row in ws.iter_rows():
-    recipient = row[0].value
-    print(recipient)
-    title = row[1].value
-    text = row[2].value
-    attachment = row[3].value
-    send_email('**', '**', recipient, title, text, attachment)
-
+#메소드 호출
+es.send_email()
 

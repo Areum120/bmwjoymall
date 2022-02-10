25d180300b2030c148788b86cd937600c80c0d28
DESKTOP-Q8FP0G2\workscombine <tjfsu120@gmail.com>
2022-02-10 15:17:22 +0900

    exe 실행파일 update

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ from gui.rpa_thread import UI
 
 # initialize The App
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    UIWindow = UI()
-    UIWindow.show()
+    try:
+        app = QApplication(sys.argv)
+        UIWindow = UI()
+        UIWindow.show()
+    except Exception as e:
+            print(e)  # 에러를 표시하기 위해서
+            input()  # 멈추게 하기 위해서
     app.exec_()
 
